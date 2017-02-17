d1c1d79b34600be50b1da87d87f8946df4e61837
cr0wbait <etcothran@gmail.com>
2017-02-17 13:33:15 -0500

    work continuing 2-17

--- js/app.js
+++ js/app.js
@@ -2,5 +2,8 @@ console.log('Prepare the ray gun!')
 
 document.querySelector('button').addEventListener('click', function(){
   console.log('FIRE AT WILL')
-
+  let ongoing = document.querySelector('.ongoing').innerHTML
+  let fillme = document.querySelector('.fillme').select('');
+  console.log(ongoing)
+  console.log(fillme)
 })
