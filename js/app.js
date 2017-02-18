ac7df4a45d1aa57fe0ce537e363f98d7dba8fa71
cr0wbait <etcothran@gmail.com>
2017-02-17 23:00:41 -0500

    finished work for 2-17

--- js/app.js
+++ js/app.js
@@ -2,7 +2,30 @@ console.log('Prepare the ray gun!')
 
 document.querySelector('button').addEventListener('click', function(){
   console.log('FIRE AT WILL')
-  let ongoing = document.querySelector('.ongoing').innerHTML
-  let fillme = document.querySelector('input').select('');
+  let ongoing = document.getElementsByClassName('ongoing')
+  let task = document.getElementsByClassName('ongoing').innerHTML
+  let fillme = document.getElementsByClassName('input')
+  let rotting = document.getElementById('downcount').innerHTML
+  let pComp = "<p class='comp'>Pomodoro Completed</p>"
+
+
   console.log(fillme)
+  console.log(ongoing)
+
+  task = fillme.value
+  fillme = ''
+
+//  let tomato = setInterval(downCount, 1000);
+
+// let downCount = function(evt){
+//     var minTotal = ;
+//     var secTotal = ;
+//     rotting = minTotal + ":" + secTotal
+//
+//     if (minTotal === '00' && secTotal === '00'){
+//       rotting.style.display = 'none'
+//       ongoing.classList.add(pComp)
+//
+//     }
+//   }
 })
