9a217e7af3ba445bcd1e41b3064f7a8e499d8213
cr0wbait <etcothran@gmail.com>
2017-02-26 16:55:58 -0500

    this is maddening

--- js/app.js
+++ js/app.js
@@ -7,25 +7,9 @@ document.querySelector('button').addEventListener('click', function(){
   let fillme = document.getElementsByClassName('input')
   let rotting = document.getElementById('downcount').innerHTML
   let pComp = "<p class='comp'>Pomodoro Completed</p>"
-
-
   console.log(fillme)
   console.log(ongoing)
-
   task = fillme.value
   fillme = ''
 
-  let tomato = setInterval(downCount, 1000);
-
-  let downCount = function(evt){
-     var minTotal = ;
-     var secTotal = ;
-     rotting = minTotal + ":" + secTotal
-
-     if (minTotal === '00' && secTotal === '00'){
-       rotting.style.display = 'none'
-       ongoing.classList.add(pComp)
-
-     }
-   }
 })
